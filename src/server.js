9439b8d412dbd04b81608394c281e29c089c85a5
Murilo Rodegheri <murilo.rodegheri@gmail.com>
2022-04-28 10:21:13 -0300

    feat: add routes, controllers and logger middleware

--- src/server.js
+++ src/server.js
@@ -1,12 +1,30 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const bootcamps = require('./routes/bootcamps.routes')
 
+// load envs
 const env = dotenv.config()
 if (env.error) {
   throw env.error
 }
 
 const app = express()
+
+const logger = (req, res, next) => {
+  const date = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '')
+
+  console.log(
+    `${date} - ${req.method} ${req.protocol}://${req.get('host')}${
+      req.originalUrl
+    }`
+  )
+  next()
+}
+
+app.use(logger)
+
+// mount routers
+app.use('/api/v1/bootcamps', bootcamps)
 
 const PORT = process.env.PORT || 5000
 
