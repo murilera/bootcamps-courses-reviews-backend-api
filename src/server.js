86bc08a4a203e396742ab313ec3687298688f7ec
Murilo Rodegheri <murilo.rodegheri@gmail.com>
2022-04-30 17:43:34 -0300

    feat: add mongodb connection

--- src/server.js
+++ src/server.js
@@ -2,12 +2,17 @@ const express = require('express')
 const dotenv = require('dotenv')
 const bootcamps = require('./routes/bootcamps.routes')
 const morgan = require('morgan')
+const colors = require('colors')
+const connectDB = require('./config/db')
 
 // load envs
 const env = dotenv.config()
 if (env.error) {
   throw env.error
 }
+
+// connect to mongodb
+connectDB()
 
 const app = express()
 
@@ -19,7 +24,16 @@ app.use('/api/v1/bootcamps', bootcamps)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 )
+
+// handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red)
+
+  server.close(() => process.exit(1))
+})
