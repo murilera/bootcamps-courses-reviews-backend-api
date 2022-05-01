766b2c424fa098f189b7f76e6f66c7aff4ff6aae
Murilo Rodegheri <murilo.rodegheri@gmail.com>
2022-05-01 12:04:08 -0300

    feat: add get single bootcamp from id from db

--- src/controllers/bootcamps.controller.js
+++ src/controllers/bootcamps.controller.js
@@ -19,11 +19,21 @@ exports.getBootcamps = async (req, res, next) => {
 // @desc    Get single bootcamp
 // @route   GET /api/v1/bootcamps/:id
 // @access  Public
-exports.getBootcamp = (req, res, next) => {
-  res.status(200).json({
-    success: true,
-    msg: `show bootcamps ${req.params.id}`
-  })
+exports.getBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findById(req.params.id)
+
+    if (!bootcamp) {
+      return res.status(400).json({ success: false })
+    }
+
+    res.status(200).json({
+      success: true,
+      data: bootcamp
+    })
+  } catch (err) {
+    res.status(400).json({ success: false })
+  }
 }
 
 // @desc    Create new bootcamp
