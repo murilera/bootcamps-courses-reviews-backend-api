6a3ab4aa735349a4e66d3d26c4f868880e70af9d
Murilo Rodegheri <murilo.rodegheri@gmail.com>
2022-05-02 13:40:42 -0300

    feat: add pagination

--- src/controllers/bootcamps.controller.js
+++ src/controllers/bootcamps.controller.js
@@ -11,7 +11,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
   const reqQuery = { ...req.query }
 
   // fields to exclude
-  const removeFields = ['select', 'sort']
+  const removeFields = ['select', 'sort', 'page', 'limit']
 
   // loop over removeFields and delete them from reqQuery
   removeFields.forEach((param) => delete reqQuery[param])
@@ -42,12 +42,36 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
     query = query.sort('-createdAt')
   }
 
+  // pagination
+  const page = parseInt(req.query.page, 10) || 1
+  const limit = parseInt(req.query.limit, 10) || 25
+  const startIndex = (page - 1) * limit
+  const endIndex = page * limit
+  const total = await Bootcamp.countDocuments()
+  query = query.skip(startIndex).limit(limit)
+
   // exec query
   const bootcamps = await query
+
+  // pagination result
+  const pagination = {}
+  if (endIndex < total) {
+    pagination.next = {
+      page: page + 1,
+      limit
+    }
+  }
+  if (startIndex > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit
+    }
+  }
 
   res.status(200).json({
     success: true,
     count: bootcamps.length,
+    pagination,
     data: bootcamps
   })
 })
