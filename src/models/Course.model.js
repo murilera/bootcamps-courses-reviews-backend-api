0f309bde3b3ef12995683cc4605e2e056f9926eb
Murilo Rodegheri <murilo.rodegheri@gmail.com>
2022-05-09 12:55:25 -0300

    feat: add calculate average cost

--- src/models/Course.model.js
+++ src/models/Course.model.js
@@ -38,4 +38,37 @@ const CourseSchema = new mongoose.Schema({
   }
 })
 
+// static method to get avg of course tuitions
+CourseSchema.statics.getAvaregeCost = async function (bootcampId) {
+  const obj = await this.aggregate([
+    {
+      $match: { bootcamp: bootcampId }
+    },
+    {
+      $group: {
+        _id: '$bootcamp',
+        averageCost: { $avg: '$tuition' }
+      }
+    }
+  ])
+
+  try {
+    await this.model('Bootcamp').findByIdAndUpdate(bootcampId, {
+      averageCost: Math.ceil(obj[0].averageCost / 10) * 10
+    })
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+// call getAvaregeCost after save
+CourseSchema.post('save', function () {
+  this.constructor.getAvaregeCost(this.bootcamp)
+})
+
+// call getAvaregeCost before remove
+CourseSchema.pre('remove', function () {
+  this.constructor.getAvaregeCost(this.bootcamp)
+})
+
 module.exports = mongoose.model('Course', CourseSchema)
