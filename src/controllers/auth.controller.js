e2a3a73363f4d343543cb21b2033724d5994ba53
Murilo Rodegheri <murilo.rodegheri@gmail.com>
2022-05-14 00:17:06 -0300

    feat: add create user route and jwt middleware in model

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -6,5 +6,18 @@ const User = require('../models/User.model')
 // @route   GET /api/v1/auth/register
 // @access  Public
 exports.register = asyncHandler(async (req, res, next) => {
-  res.status(200).json({ success: true })
+  const { name, email, password, role } = req.body
+
+  // create user
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role
+  })
+
+  // create token
+  const token = user.getSignedJwtToken()
+
+  res.status(200).json({ success: true, token })
 })
