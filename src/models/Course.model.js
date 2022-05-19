b0d37b0bd1059741c289e2572016dfbc1b53f11c
Murilo Rodegheri <murilo.rodegheri@gmail.com>
2022-05-18 23:38:13 -0300

    feat: add avg calculate for reviews rating

--- src/models/Course.model.js
+++ src/models/Course.model.js
@@ -67,13 +67,11 @@ CourseSchema.statics.getAverageCost = async function (bootcampId) {
 }
 
 // Call getAverageCost after save
-
 CourseSchema.post('save', async function () {
   await this.constructor.getAverageCost(this.bootcamp)
 })
 
 // Call getAverageCost before remove
-
 CourseSchema.pre('remove', async function () {
   await this.constructor.getAverageCost(this.bootcamp)
 })
--- src/models/Review.model.js
+++ src/models/Review.model.js
@@ -33,6 +33,39 @@ const ReviewSchema = new mongoose.Schema({
   }
 })
 
+// static method to get avg rating and save
+ReviewSchema.statics.getAverageRating = async function (bootcampId) {
+  const obj = await this.aggregate([
+    {
+      $match: { bootcamp: bootcampId }
+    },
+    {
+      $group: {
+        _id: '$bootcamp',
+        averageRating: { $avg: '$rating' }
+      }
+    }
+  ])
+
+  try {
+    await this.model('Bootcamp').findByIdAndUpdate(bootcampId, {
+      averageRating: obj[0].averageRating
+    })
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+// Call getAverageRating after save
+ReviewSchema.post('save', async function () {
+  await this.constructor.getAverageRating(this.bootcamp)
+})
+
+// Call getAverageRating before remove
+ReviewSchema.pre('remove', async function () {
+  await this.constructor.getAverageRating(this.bootcamp)
+})
+
 // prevent user from submitting more than one review per bootcamp
 ReviewSchema.index({ bootcamp: 1, user: 1 }, { unique: true })
 
