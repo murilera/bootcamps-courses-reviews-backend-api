f194a1681ff44ab41ee3692dab1264e0639fc4b7
Murilo Rodegheri <murilo.rodegheri@gmail.com>
2022-05-18 23:35:52 -0300

    feat: create add review route and add index to preventing user to submit more than one review

--- src/controllers/reviews.controller.js
+++ src/controllers/reviews.controller.js
@@ -32,12 +32,38 @@ exports.getReview = asyncHandler(async (req, res, next) => {
   })
 
   if (!review) {
-    next(
+    return next(
       new ErrorResponse(`No review found with the id of ${req.params.id}`, 404)
     )
   }
 
   return res.status(200).json({
+    success: true,
+    data: review
+  })
+})
+
+// @desc    Add review
+// @route   POST /api/v1/bootcamps/:bootcampId/reviews
+// @access  Public
+exports.addReview = asyncHandler(async (req, res, next) => {
+  req.body.bootcamp = req.params.bootcampId
+  req.body.user = req.user.id
+
+  const bootcamp = await Bootcamp.findById(req.params.bootcampId)
+
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(
+        `No bootcamp with the id of ${req.params.bootcampId}`,
+        404
+      )
+    )
+  }
+
+  const review = await Review.create(req.body)
+
+  return res.status(201).json({
     success: true,
     data: review
   })
--- src/routes/reviews.routes.js
+++ src/routes/reviews.routes.js
@@ -3,17 +3,24 @@ const express = require('express')
 const Review = require('../models/Review.model')
 const advancedResults = require('../middleware/advancedResults.middleware.')
 const { protect, authorize } = require('../middleware/auth.middleware')
-const { getReviews, getReview } = require('../controllers/reviews.controller')
+const {
+  getReviews,
+  getReview,
+  addReview
+} = require('../controllers/reviews.controller')
 
 const router = express.Router({ mergeParams: true })
 
-router.route('/').get(
-  advancedResults(Review, {
-    path: 'bootcamp',
-    select: 'name description'
-  }),
-  getReviews
-)
+router
+  .route('/')
+  .get(
+    advancedResults(Review, {
+      path: 'bootcamp',
+      select: 'name description'
+    }),
+    getReviews
+  )
+  .post(protect, authorize('user', 'admin'), addReview)
 
 router.route('/:id').get(getReview)
 
