431483a3d1508df6bdf1fe9792cd138743c913b4
Murilo Rodegheri <murilo.rodegheri@gmail.com>
2022-05-18 23:04:27 -0300

    feat: add reviews data

--- src/seeder.js
+++ src/seeder.js
@@ -11,6 +11,7 @@ dotenv.config()
 const Bootcamp = require('./models/Bootcamp.model')
 const Course = require('./models/Course.model')
 const User = require('./models/User.model')
+const Review = require('./models/Review.model')
 
 // connect to db
 mongoose.connect(process.env.MONGO_URL, {
@@ -28,12 +29,16 @@ const courses = JSON.parse(fs.readFileSync(coursesPath, 'utf-8'))
 const usersPath = path.join(__dirname, '/_data/users.json')
 const users = JSON.parse(fs.readFileSync(usersPath, 'utf-8'))
 
+const reviewsPath = path.join(__dirname, '/_data/reviews.json')
+const reviews = JSON.parse(fs.readFileSync(reviewsPath, 'utf-8'))
+
 // import to db
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps)
     await Course.create(courses)
     await User.create(users)
+    await Review.create(reviews)
 
     console.log('Data imported...'.green.inverse)
     process.exit()
@@ -48,6 +53,7 @@ const deleteData = async () => {
     await Bootcamp.deleteMany()
     await Course.deleteMany()
     await User.deleteMany()
+    await Review.deleteMany()
 
     console.log('Data destroyed...'.red.inverse)
     process.exit()
