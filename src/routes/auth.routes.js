95ff6423cd3b3ac72d736ddf8da9fcddbc628b0a
Murilo Rodegheri <murilo.rodegheri@gmail.com>
2022-05-19 00:09:22 -0300

    feat: add logout user route

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -6,7 +6,8 @@ const {
   forgotPassword,
   resetPassword,
   updateDetails,
-  updatePassword
+  updatePassword,
+  logout
 } = require('../controllers/auth.controller')
 const { protect } = require('../middleware/auth.middleware')
 
@@ -14,6 +15,7 @@ const router = express.Router()
 
 router.post('/register', register)
 router.post('/login', login)
+router.get('/logout', logout)
 router.get('/me', protect, getMe)
 router.put('/updatedetails', protect, updateDetails)
 router.put('/updatepassword', protect, updatePassword)
