6b2a98b37c6bc53b9603862ae86831a24fb12b9c
Murilo Rodegheri <murilo.rodegheri@gmail.com>
2022-05-18 23:00:10 -0300

    feat: add get single review

--- src/routes/reviews.routes.js
+++ src/routes/reviews.routes.js
@@ -3,7 +3,7 @@ const express = require('express')
 const Review = require('../models/Review.model')
 const advancedResults = require('../middleware/advancedResults.middleware.')
 const { protect, authorize } = require('../middleware/auth.middleware')
-const { getReviews } = require('../controllers/reviews.controller')
+const { getReviews, getReview } = require('../controllers/reviews.controller')
 
 const router = express.Router({ mergeParams: true })
 
@@ -14,5 +14,7 @@ router.route('/').get(
   }),
   getReviews
 )
+
+router.route('/:id').get(getReview)
 
 module.exports = router
